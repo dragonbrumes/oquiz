d82b8c22c8fc5a96caccad47974646cb1d93444b
Stephane aal <dragonbrumes@gmail.com>
2018-05-22 22:53:55 +0200

    v1.1 Play system, login/logout, embellishment, No Bonus,remove some usless stuff

--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -13,7 +13,7 @@
             <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Adresse email">
           </div>
           <div class="form-group">
-            <label for="exampleInputPassword1">Mot de passe quizoclock</label>
+            <label for="exampleInputPassword1">Mot de passe</label>
             <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
